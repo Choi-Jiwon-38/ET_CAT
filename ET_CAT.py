ff4fa737ad48f5d37ae521354abb970337c89725
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2021-12-01 10:53:54 +0900

    Update ET_CAT.py
    Function Update
    - entering_player_word()
    - update_wordList()
    - first_start_game()

--- ET_CAT.py
+++ ET_CAT.py
@@ -10,7 +10,7 @@
 
 # ================ QtDesigner로 GUI를 작성하였습니다 ============================
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from catFunction import checkingDictionary, end_talk, game_start_setting, loadWordList, updateWordList
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -18,36 +18,48 @@ class Ui_MainWindow(object):
         MainWindow.resize(371, 555)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.labelEnglish = QtWidgets.QLabel(self.centralwidget)
         self.labelEnglish.setGeometry(QtCore.QRect(20, 10, 161, 51))
         font = QtGui.QFont()
         font.setFamily("Bodoni MT Black")
         font.setPointSize(30)
+        
         self.labelEnglish.setFont(font)
         self.labelEnglish.setObjectName("labelEnglish")
+        
         self.labelKorean = QtWidgets.QLabel(self.centralwidget)
         self.labelKorean.setGeometry(QtCore.QRect(170, 60, 191, 21))
         font = QtGui.QFont()
         font.setFamily("휴먼매직체")
         font.setPointSize(12)
+        
         self.labelKorean.setFont(font)
         self.labelKorean.setObjectName("labelKorean")
+        
         self.screenEndTalk = QtWidgets.QTextBrowser(self.centralwidget)
         self.screenEndTalk.setGeometry(QtCore.QRect(20, 310, 331, 121))
         self.screenEndTalk.setObjectName("screenEndTalk")
+        
         self.screenCat = QtWidgets.QGraphicsView(self.centralwidget)
         self.screenCat.setGeometry(QtCore.QRect(20, 100, 331, 192))
         self.screenCat.setObjectName("screenCat")
+        
         self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
         self.plainTextEdit.setGeometry(QtCore.QRect(20, 450, 281, 41))
         self.plainTextEdit.setObjectName("plainTextEdit")
-        self.buttonInout = QtWidgets.QPushButton(self.centralwidget)
-        self.buttonInout.setGeometry(QtCore.QRect(310, 450, 41, 41))
+        
+        self.buttonInput = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonInput.setGeometry(QtCore.QRect(310, 450, 41, 41))
         font = QtGui.QFont()
         font.setFamily("BM JUA_TTF")
         font.setPointSize(15)
-        self.buttonInout.setFont(font)
-        self.buttonInout.setObjectName("buttonInout")
+        self.buttonInput.setFont(font)
+        self.buttonInput.setObjectName("buttonInput")
+        # 함수 연결 __ buttonInput
+        self.buttonInput.clicked.connect(self.entering_player_word)
+
+
         self.buttonUpgrade = QtWidgets.QPushButton(self.centralwidget)
         self.buttonUpgrade.setGeometry(QtCore.QRect(260, 20, 81, 31))
         font = QtGui.QFont()
@@ -55,15 +67,20 @@ class Ui_MainWindow(object):
         font.setPointSize(11)
         self.buttonUpgrade.setFont(font)
         self.buttonUpgrade.setObjectName("buttonUpgrade")
+        # 함수 연결 __ buttonUpgrade
+        self.buttonUpgrade.clicked.connect(self.update_wordList)
+
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 371, 21))
         self.menubar.setObjectName("menubar")
+        
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+        
         MainWindow.setStatusBar(self.statusbar)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -78,9 +95,33 @@ class Ui_MainWindow(object):
 "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
-        self.buttonInout.setText(_translate("MainWindow", "♥"))
+        self.buttonInput.setText(_translate("MainWindow", "♥"))
         self.buttonUpgrade.setText(_translate("MainWindow", "UPDATE"))
+# =============================================================================
+    
+    def entering_player_word(self):
+        print('InputButton Clicked!') # debug1
+        word = self.plainTextEdit.toPlainText()
+        self.screenEndTalk.append("플레이어: " + word)
+        if word == "시작!":
+            self.first_start_game()
+        else:
+            screenWord = end_talk(word)
+            self.screenEndTalk.append("고양이: " + screenWord)
 
+
+    def update_wordList(self):
+        print('UpdateButton Clicked') # debug2
+        updateWordList()
+        
+
+    def displaying_cat_image(self):
+        pass
+    
+    def first_start_game(self):
+        self.screenEndTalk.append("고양이: 그러면 나 먼저 시작할게!")
+        word = game_start_setting()
+        self.screenEndTalk.append("고양이: 음.... " + word)
 
 if __name__ == "__main__":
     import sys
@@ -90,4 +131,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-# =============================================================================
